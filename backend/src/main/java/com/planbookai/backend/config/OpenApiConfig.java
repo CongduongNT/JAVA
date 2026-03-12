d682be7f76f44d1b54e709b68f346fd5885d43c9
Nguyen Trung Cong Duong <nguyentrungcongduong@gmail.com>
2026-03-12 13:04:10 +0700

    KAN-8 setup swaggerr and custom .yml

---
+++ backend/src/main/java/com/planbookai/backend/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.planbookai.backend.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI planbookAiOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("PlanbookAI API")
+                        .description("AI-powered tools for high school teachers")
+                        .version("v1.0.0")
+                        .contact(new Contact()
+                                .name("PlanbookAI Team")
+                                .email("support@planbookai.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://springdoc.org")));
+    }
+}
