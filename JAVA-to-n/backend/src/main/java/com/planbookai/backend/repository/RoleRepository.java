72ac58ddc22ec986f1c3458a93b31b72fc9aa319
CongduongNT <njack8151@gmail.com>
2026-03-16 04:07:26 +0700

    remove nested repo

--- JAVA-to-n/backend/src/main/java/com/planbookai/backend/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.planbookai.backend.repository;
-
-import com.planbookai.backend.model.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-    Optional<Role> findByName(Role.RoleName name);
-}
