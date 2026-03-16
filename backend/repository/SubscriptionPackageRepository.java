2e85b07c0f960ee791283faa24ea37dc3b4caeca
NNGOCTOAN <njack8151@gmail.com>
2026-03-16 20:19:14 +0700

    Delete backend/repository directory

--- backend/repository/SubscriptionPackageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.planbookai.backend.repository;
-
-import com.planbookai.backend.model.entity.SubscriptionPackage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubscriptionPackageRepository extends JpaRepository<SubscriptionPackage, Integer> {
-}
