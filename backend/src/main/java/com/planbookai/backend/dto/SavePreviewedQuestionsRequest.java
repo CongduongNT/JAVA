904ec5bc80a9b10ab68bb59b8d142aac0ea3caa5
DucPhat-stu <phatnd2637@ut.edu.vn>
2026-04-05 14:24:25 +0700

    refactor(SavePreviewedQuestionsRequest): reorganize getters and setters for clarity

--- backend/src/main/java/com/planbookai/backend/dto/SavePreviewedQuestionsRequest.java
+++ backend/src/main/java/com/planbookai/backend/dto/SavePreviewedQuestionsRequest.java
@@ -11,4 +11,20 @@ public class SavePreviewedQuestionsRequest {
 
     private Integer bankId;
     private List<QuestionDTO> questions;
+
+    public Integer getBankId() {
+        return bankId;
+    }
+
+    public void setBankId(Integer bankId) {
+        this.bankId = bankId;
+    }
+
+    public List<QuestionDTO> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<QuestionDTO> questions) {
+        this.questions = questions;
+    }
 }
